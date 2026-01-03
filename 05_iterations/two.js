f737b35280a858c3d498d2ec90754a02929bede2
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2026-01-03 06:20:53 +0530

    Add while loop examples for index iteration and array traversal in two.js

---
+++ 05_iterations/two.js
@@ -0,0 +1,15 @@
+// two.js
+
+let idx = 0;
+while (idx <= 5) {
+  console.log(`index is ${idx}`);
+  idx++;
+}
+
+let arr = ["js", "cpp", "py"];
+let i = 0;
+
+while (i < arr.length) {
+  console.log(arr[i]);
+  i++;
+}
