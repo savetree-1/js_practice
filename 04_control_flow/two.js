1d3b943f13d37d8b44c4c99ff6337467b282db3a
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2026-01-03 06:12:20 +0530

    Add switch statement example for months
    Introduces two.js with a switch statement that logs the month name based on the value of 'mon'.

---
+++ 04_control_flow/two.js
@@ -0,0 +1,21 @@
+// two.js
+
+const mon = 3;
+
+switch (mon) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("February");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  default:
+    console.log("Invalid month");
+    break;
+}
