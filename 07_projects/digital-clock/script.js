b2d476911ce59d2839663dffaa3a9a4e7e6cbc7c
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2026-01-31 02:23:36 +0000

    Refactor clock update mechanism to use requestAnimationFrame for smoother updates

--- 07_projects/digital-clock/script.js
+++ 07_projects/digital-clock/script.js
@@ -1,8 +1,14 @@
 const clock = document.getElementById('clock');
 // const clock = document.querySelector('#clock')
 
-setInterval(function () {
-  let date = new Date();
-  clock.innerHTML = date.toLocaleTimeString();
-}, 1000);
+// setInterval(function () {
+//   let date = new Date();
+//   clock.innerHTML = date.toLocaleTimeString();
+// }, 1000);
 
+
+function updateClock() {
+  clock.innerHTML = new Date().toLocaleTimeString();
+  requestAnimationFrame(updateClock); 
+}
+updateClock();
